4183f16a9cb9b8d7d54984de8e4f2f809de39dac
nuwrldnf8r <nuwrldnf8r@gmail.com>
2023-12-22 20:45:36 +0200

    changed to a select on jobs

--- src/components/job_select.js
+++ src/components/job_select.js
@@ -5,20 +5,16 @@ const JobSelect = props => {
         'Farmer','Farm Manager','Field Manager','Factory Manager','Receiving Manager','Harvester'
     ]
     const liAr = jobs.map((j,i)=>(
-        <li  key={i} class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                <div class="flex items-center ps-3">
-                    <input id={j} type="radio" value={j} name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                    <label for="Farmer" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{j}</label>
-                </div>
-        </li>
+        <option value={j} key={i}>{j}</option>
     ))
 
     return (
         <>
-        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">I am a..</label>
-        <ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white" onChange={e=>props.onSelect(e.target.value)}>
-            {liAr}
-        </ul>
+        <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">I am a..</label>
+        <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={e=>props.onSelect(e.target.value)}>
+        {liAr}
+        </select>
+       
         </>
     )
 }
